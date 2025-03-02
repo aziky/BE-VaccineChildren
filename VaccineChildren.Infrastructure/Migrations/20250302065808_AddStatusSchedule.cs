ecf67c6d86aefd3e63e0ed37126e98bfbc827aa9
HCM24_CPL_NET_14_TranQuocMinh <minhtqse183954@fpt.edu.vn>
2025-03-02 15:02:34 +0700

    Migration

---
+++ VaccineChildren.Infrastructure/Migrations/20250302065808_AddStatusSchedule.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VaccineChildren.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddStatusSchedule : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "pre_vaccine_checkup",
+                table: "schedule",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AddColumn<string>(
+                name: "status",
+                table: "schedule",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+        }
+    }
+}
