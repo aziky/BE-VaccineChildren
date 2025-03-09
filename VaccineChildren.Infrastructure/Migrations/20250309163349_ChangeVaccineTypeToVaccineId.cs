6b6fbc0cc94d88194a76487f683bfb5c2ee150a9
Nguyen Duc Tuyen <ndtuyen0604@gmail.com>
2025-03-10 00:08:01 +0700

    migration

---
+++ VaccineChildren.Infrastructure/Migrations/20250309163349_ChangeVaccineTypeToVaccineId.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VaccineChildren.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeVaccineTypeToVaccineId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "vaccine_type",
+                table: "schedule");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "vaccine_id",
+                table: "schedule",
+                type: "uuid",
+                maxLength: 255,
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "vaccine_id",
+                table: "schedule");
+
+            migrationBuilder.AddColumn<string>(
+                name: "vaccine_type",
+                table: "schedule",
+                type: "character varying(255)",
+                maxLength: 255,
+                nullable: true);
+        }
+    }
+}
